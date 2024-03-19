49fc36484b423c1248ba5a89b4f0732e47cdc061
LeonardoNTI <leonardo.giannini@elev.ga.ntig.se>
2024-03-19 20:49:15 +0100

    Börjat på sida 3 samt laggt till java till min nav på alla sidor
    HAMBURGAR MENY!!!

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 
+const menu_btn = document.querySelector('.hamburger');
+const navigator = document.querySelector('.navigator');
 
-document.querySelector('.menu').addEventListener('click', function() {
-    document.querySelector('.hamburger-menu').classList.toggle('active');
+menu_btn.addEventListener('click', function () {
+    menu_btn.classList.toggle('is-active');
+    navigator.classList.toggle('is-active');
 });
